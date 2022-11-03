a48ff12998e5007294fc5cb302f6e9ad8df575d6
AnuroopAnuz21 <anuroopprasad731@gmail.com>
2022-11-03 09:32:09 +0530

    partially

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ var db = mongoose.connection;
 db.on('error',()=>console.log("Error in Connecting to Database"));
 db.once('open',()=>console.log("Connected to Database"))
 
+
 app.post("/sign_up",(req,res)=>{
     var username = req.body.username;
     var room = req.body.room;
@@ -43,39 +44,46 @@ app.post("/sign_up",(req,res)=>{
 })
 
 
-app.get("/",(req,res)=>{
-    res.set({
-        "Allow-access-Allow-Origin": '*'
-    })
-    return res.redirect('index.html');
-}).listen(3000);
+app.post("/newadmin",(req,res)=>{
+    var adminname = req.body.adminname;
+    var adminpassword = req.body.adminpassword;
 
+    var data = {
+        "adminname": adminname,
+        "adminpassword" : adminpassword
+    }
 
-console.log("Listening on PORT 3000");
+    db.collection('admin').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
 
+    return res.redirect('admin.html')
 
+})
 
+app.post("/viewsignup",(req,res)=>{
+   
+    var username = req.body.username;
+    var room = req.body.room;
+    var password = req.body.password;
 
+  
+    db.collection('users').findOne({username:username},
+        (error,data)=>{
+            if(error){
+                res.send(error)
 
+            }
+            if(data){
+                console.log(data)
+                this.data=data
+                if(room==data.room){
+                    if(password == data.password){
 
-const path = require('path');
-const http = require('http');
-
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
-const {
-     userJoin,
-     getCurrentUser,
-     userLeave,
-     getRoomUsers 
-    }= require('./utils/users');
-
-const server = http.createServer(app);
-const io = socketio(server);
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
+                       
 const botName = 'JUTELLA';
 
 io.on('connection', (socket) => {
@@ -85,6 +93,7 @@ io.on('connection', (socket) => {
         socket.join(user.room);
   
         socket.emit('message', formatMessage(botName, `Welcome to the ${user.room}!`));
+        
 
         socket.broadcast.to(user.room).emit('message', formatMessage(botName,  `${user.username} has joined the chat`));
 
@@ -126,6 +135,105 @@ io.on('connection', (socket) => {
 
 
 });
+
+                        return res.redirect('chat.html')
+                    }else{
+                        
+                        res.send("password doesnot match")
+                    }
+                }else{
+                    res.send("room doesnot match")
+                }
+            }else{
+                res.send("username doesnot exists")
+            }
+        })
+    })
+
+  
+     
+
+    app.post("/easylogin",(req,res)=>{
+        var adminname = req.body.adminname;
+        var adminpassword = req.body.adminpassword;
+        console.log(req.body)
+
+        console.log(req.body.adminname)
+        console.log(req.body.adminpassword)
+    
+        
+      
+        db.collection('admin').findOne({adminname:adminname},
+            (error,data)=>{
+                if(error){
+                    res.send(error)
+    
+                }
+                if(data){
+                    console.log(data)
+                    this.data=data
+                    
+                    if(adminpassword==data.adminpassword){
+                        
+                            return res.redirect('adminlogin.html')
+                        }else{
+                            res.send("password doesnot match")
+                        }
+                    }else{
+                        res.send("admin doesnot exist")
+                    }
+                
+               
+            })
+        })
+        
+
+        //feedback database
+     
+
+        app.post("/feedback",(req,res)=>{
+            var msg = req.body.msg;
+        
+            var data = {
+                "msg": msg
+            }
+        
+            db.collection('feedback').insertOne(data,(err,collection)=>{
+                if(err){
+                    throw err;
+                }
+                console.log("Record Inserted Successfully");
+            });
+        
+            return res.redirect('feedbacknotice.html')
+        
+        })
+        
+        
+
+
+
+
+//starting socket
+const path = require('path');
+const http = require('http');
+
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+     userJoin,
+     getCurrentUser,
+     userLeave,
+     getRoomUsers 
+    }= require('./utils/users');
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 
 const PORT = process.env.PORT || 5000;
